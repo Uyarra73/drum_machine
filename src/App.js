ab63ea7974dc41fd9eabf2893b38439d8fd53d04
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-08-29 11:16:03 +0900

    Sounds implemented

--- src/App.js
+++ src/App.js
@@ -6,49 +6,54 @@ function App() {
     {
       keyCode: 81,
       text: "Q",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"
     },
     {
       keyCode: 87,
       text: "W",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"
     },
     {
       keyCode: 69,
       text: "E",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"
     },
     {
       keyCode: 65,
       text: "A",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"
     },
     {
       keyCode: 83,
       text: "S",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"
     },
     {
       keyCode: 68,
       text: "D",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"
     },
     {
       keyCode: 90,
       text: "Z",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"
     },
     {
       keyCode: 88,
       text: "X",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"
     },
     {
       keyCode: 67,
       text: "C",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"
     }
   ];
+
+  const playSound = (selector) => {
+    const audio = document.getElementById(selector);
+    audio.play();
+  }
 
   return (
     <div className="App">
@@ -59,11 +64,12 @@ function App() {
           {drumKeys.map(drumPad => {
             return (
                 <div className='drum-pad'
-                id={drumPad.text}>{drumPad.text}</div>
+                id={drumPad.src} onClick={() => playSound(drumPad.text)}>{drumPad.text}
+                <audio src={drumPad.src} className='clip' id={drumPad.text}></audio>
+                </div>
             )
-          })}  
+          })}
         </div>
-        
       </div>
     </div>
   );
